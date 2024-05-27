a0a268d23b2b7788255a4c55454c4d0cef60e116
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-05-27 11:18:51 +0200

    17. Add documentation to FifoRhymer class

--- src/main/java/edu/kis/vh/nursery/FifoRhymer.java
+++ src/main/java/edu/kis/vh/nursery/FifoRhymer.java
@@ -1,9 +1,20 @@
 package edu.kis.vh.nursery;
 
+/**
+ * FifoRhymer is a class that extends DefaultCountingOutRhymer class
+ * It has DefaultCountingOutRhymer attribute
+ * It overrides the countOut method to provide a FIFO functionality
+ */
 public class FifoRhymer extends DefaultCountingOutRhymer {
 
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+    /**
+     * Overrides the countOut method from DefaultCountingOutRhymer.
+     * It provides a FIFO behavior by using a temporary DefaultCountingOutRhymer.
+     *
+     * @return the first integer that was counted in.
+     */
     @Override
     public int countOut() {
         while (!callCheck())
